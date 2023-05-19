ba3b5bf40e72a32109b89bcc9259c38100d4ad46
AlexeyMachehin <99137228+AlexeyMachehin@users.noreply.github.com>
2023-05-20 01:59:39 +0500

    fix error

--- src/utils/generateRecommendedBook.ts
+++ src/utils/generateRecommendedBook.ts
@@ -3,13 +3,13 @@ import { IBook } from '@/types/IBook';
 export function generateRecommendedBook(books: IBook[]): IBook | null {
   const currentYear = new Date().getUTCFullYear();
 
-  const sortedByYear = books.filter(book => book.year <= currentYear - 3);
+  const sortedByYear = books.filter(book => book.year && book.year <= currentYear - 3);
 
   if (sortedByYear.length === 0) {
     return null;
   }
 
-  const maxRating = Math.max(...sortedByYear.map(book => book.rating));
+  const maxRating = Math.max(...sortedByYear.map(book => book.rating ?? 0));
 
   const sortedByRating = sortedByYear.filter(book => book.rating === maxRating);
 
